1a3ace7fcfca08946fc3e83372b5f08e89a5b8e4
Rajesh Pillai <pillai.rajesh@gmail.com>
2018-05-15 12:04:49 +0530

    Converting game.js into es6 class

--- game.js
+++ game.js
@@ -1,10 +1,17 @@
-document.addEventListener("keyup", startGame);
-
-function startGame(e) {
-    if (e.keyCode === 32) {  // space key
-        document.querySelector(".startup").style.display = "none";
-        document.getElementById("canvas-wrapper").style.display = 'block';
-        document.getElementById("canvas").style.display='block';
-        document.removeEventListener("keyup", startGame);
+class Game {
+    constructor() {
+        this.startGame = this.startGame.bind(this);
+        document.addEventListener("keyup", this.startGame)
+    }
+    startGame(e) {
+        if (e.keyCode === 32) {  // space key
+            document.querySelector(".startup").style.display = "none";
+            document.getElementById("canvas-wrapper").style.display = 'block';
+            document.getElementById("canvas").style.display='block';
+            document.removeEventListener("keyup", this.startGame);
+        }
     }
 }
+
+
+let game = new Game();
